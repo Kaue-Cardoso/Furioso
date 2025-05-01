87a0a73fcc303f9e7188c1901cace403477bdebc
Kauê Cardoso <kauekauada@hotmail.com>
2025-05-01 10:19:22 -0300

    Atualizações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", // Rota padrão (localhost:4200/)
+        component: HomeComponent
+    },
+    {
+        path: "login", // Rota para login (localhost:4200/login)
+        component: LoginComponent
+    }
+];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
